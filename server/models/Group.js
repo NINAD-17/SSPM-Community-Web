ed637bf1fccb2b2678cfc01cab6611c46ee0aa54
Ninad-17 <ninadkdhulap17@outlook.com>
2024-02-25 21:32:05 +0530

    create Schema for group and post for the groups

---
+++ server/models/Group.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const GroupSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        min: 2,
+        max: 50
+    },
+    description: {
+        type: String,
+        required: true,
+        max: 400
+    },
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    groupPosts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "GroupPost"
+    }]
+})
---
+++ server/models/GroupPost.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const GroupPostSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    description: String,
+    picturePath: String, // Post image
+    userPicturePath: String, // Profile image
+    likes: {
+        type: Map,  // Instead of array use map it's efficient.
+        of: Boolean // It checks if the user Id is exists in the map and value is true if it's exist.
+    },
+    comments: {
+        type: Array,
+        default: []
+    },
+    group: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Group'
+    }
+})
