0d6212a13aa3f18c44b548b6d87a0474e7257080
Ninad-17 <ninadkdhulap17@outlook.com>
2024-02-25 11:47:56 +0530

    Setup SSPM community project and connect with mongoDB

---
+++ server/index.js
@@ -0,0 +1,25 @@
+// Packages
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Node Native Packages
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Configurations
+// -- Path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// -- dotenv
+dotenv.config();
+// -- Initialize Express App instance
+const app = express();
+
+// Mongoose Setup and listening on port
+const PORT = 3000;
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    })
+    .catch((error) => console.log(`{error} \n:( Unable to connect!`));
